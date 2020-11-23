734d118ef68920de671ecaac9d7320b58e531b36
Adam Hoelscher <adam.hoelscher@gmail.com>
2020-11-22 17:34:47 -0800

    add comments

--- dungeonBlaster.py
+++ dungeonBlaster.py
@@ -33,13 +33,20 @@ def dungeonBlaster(m):
 
                 square = m[nx][ny]
 
+                # if we've found the exit, stop
                 if square == '^':
                     return True
+
+                # if it's an open space we can expand it later
                 elif square == ' ':
                     spaces.add(pos)
+
+                # if we found a key, pick it up and now it's a regular space
                 elif square in ascii_lowercase:
                     keys.add(square)
                     spaces.add(pos)
+
+                # if we found a door, add it to the list of doors to check
                 elif square in ascii_uppercase:
                     doors[square] = pos
 
