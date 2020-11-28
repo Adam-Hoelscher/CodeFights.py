2cdc5d9f144835413dd5fa932b850164191a04a4
Adam Hoelscher <adam.hoelscher@gmail.com>
2020-11-27 17:10:19 -0800

    PEP20[12]

--- dungeonBlaster.py
+++ dungeonBlaster.py
@@ -24,39 +24,39 @@ def dungeonBlaster(m):
     while spaces:
 
         # take a step in each direction
-            x, y = spaces.pop()
-            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        x, y = spaces.pop()
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
 
             pos = (x + dx) % h, (y + dy) % w
             
-                if pos in seen:
-                    continue
+            if pos in seen:
+                continue
 
             square = map[pos]
 
-                # if we've found the exit, stop
-                if square == '^':
-                    return True
+            # if we've found the exit, stop
+            if square == '^':
+                return True
 
-                # if it's an open space we can expand it later
-                elif square == ' ':
-                    spaces.add(pos)
+            # if it's an open space we can expand it later
+            elif square == ' ':
+                spaces.add(pos)
 
-                # if we found a key, pick it up and now it's a regular space
-                elif square in ascii_lowercase:
-                    keys.add(square)
-                    spaces.add(pos)
+            # if we found a key, pick it up and now it's a regular space
+            elif square in ascii_lowercase:
+                keys.add(square)
+                spaces.add(pos)
 
-                # if we found a door, add it to the list of doors to check
-                elif square in ascii_uppercase:
-                    doors[square] = pos
+            # if we found a door, add it to the list of doors to check
+            elif square in ascii_uppercase:
+                doors[square.lower()] = pos
 
-                seen.add(pos)
+            seen.add(pos)
 
         # open any doors for which we have found a key
-        for k in keys:
-            if (D := k.upper()) in doors:
-                # if we've opened the door, it's now a space
-                spaces.add(doors.pop(D))
+        pairs = keys.intersection(doors)
+        for k in pairs:
+            spaces.add(doors.pop(k))
+            keys.remove(k)
 
     return False
