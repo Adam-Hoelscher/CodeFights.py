557de378b4091b9b31e8742c60aea20812b5dea2
Adam Hoelscher <adam.hoelscher@gmail.com>
2020-11-27 17:40:48 -0800

    PEP20[6] rather than PEP8

--- areFollowingPatterns.py
+++ areFollowingPatterns.py
@@ -1,29 +1,12 @@
 def areFollowingPatterns(strings, patterns):
 
-    hash1 = dict()
-    hash2 = dict()
+    hash1 = {}
+    hash2 = {}
 
     for i, (x, y) in enumerate(zip(strings, patterns)):
-        try:
-            p1 = hash1[x]
-        except:
-            p1 = i
-            hash1[x] = i
-        try:
-            p2 = hash2[y]
-        except:
-            p2 = i
-            hash2[y] = i
+        p1 = hash1[x] = hash1.get(x, i)
+        p2 = hash2[y] = hash2.get(y, i)
         if p1 != p2:
-            return(False)
+            return False
 
-    return(True)
-
-if __name__ == '__main__':
-    strings= ["cat",
-              "dog",
-              "doggy"]
-    patterns= ["a",
-               "b",
-               "b"]
-    print(areFollowingPatterns(strings,patterns))
+    return True 
