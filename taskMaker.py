3549e61ba224c9d3c32b99e98cd36991dd184361
Adam Hoelscher <adam.hoelscher@gmail.com>
2020-11-30 10:53:58 -0800

    more pythonic

--- taskMaker.py
+++ taskMaker.py
@@ -1,30 +1,13 @@
 def taskMaker(source, challengeId):
 
-    chalString = '//DB ' + str(challengeId) + '//'
+    challenge_string = f'//DB {challengeId}//'
 
     out = []
     for ln in source:
-        if ln.strip()[0:2]=='//':
-            if ln.strip()[0:len(chalString)]==chalString:
-                out.pop()
-                out.append(ln.replace(chalString,''))
-        else:
+        if ln.strip().startswith(challenge_string):
+            out.pop()
+            out.append(ln.replace(challenge_string, ''))
+        elif not ln.strip().startswith('//'):
             out.append(ln)
 
-    return(out)
-
-if __name__=='__main__':
-    source = ["abacaba  dabacaba",
-              "//DB 5//abacaba  abacabadabacaba",
-              "aaaaaaaa",
-              "//DB 3//lalalala",
-              "",
-              "a",
-              "         ",
-              "      a     ",
-              "codefights",
-              "//DB 1//codefights is awesome",
-              "//DB 2//    spaces! ! ! ! !  ",
-              "//DB 999//reallybignumber"]
-    challengeId = 999
-    print(taskMaker(source, challengeId))
+    return out
